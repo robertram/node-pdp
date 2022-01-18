3be3d0dfe66b271958fc3846b2308f7293e20c26
crooks-company <37461123+robertram@users.noreply.github.com>
2022-01-18 00:36:17 -0600

    sample result changes

--- index.js
+++ index.js
@@ -1,39 +1,11 @@
 const express = require("express");
 const app = express();
+const sampleResult = require("./sampleResult.json");
 
-// Defining get request at '/' route
 app.get("/", function (req, res) {
-  res.json({
-    number: 1,
-  });
+  res.json(sampleResult);
 });
 
-// Defining get request at '/multiple' route
-app.get("/multiple", function (req, res) {
-  res.json({
-    number: 1,
-    name: "John",
-    gender: "male",
-  });
-});
-
-// Defining get request at '/array' route
-app.get("/array", function (req, res) {
-  res.json([
-    {
-      number: 1,
-      name: "John",
-      gender: "male",
-    },
-    {
-      number: 2,
-      name: "Ashley",
-      gender: "female",
-    },
-  ]);
-});
-
-// Setting the server to listen at port 3000
-app.listen(3000, function (req, res) {
-  console.log("Server is running at port 3000");
+app.listen(4000, function (req, res) {
+  console.log("Server is running at port 4000");
 });
