b543ef0aeff27603a06d88df51bee0bce84d4005
Robert Ramirez <robert@pancake.live>
2022-02-01 22:23:52 -0600

    empty sample results added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const sampleResult = require("./sampleResult.json");
+const emptySampleResult = require("./emptySampleResult.json");
 
 app.get("/", function (req, res) {
   res.json(sampleResult);
+  //res.json(emptySampleResult);
 });
 
 app.listen(4000, function (req, res) {
